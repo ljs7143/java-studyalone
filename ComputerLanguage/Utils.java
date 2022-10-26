093ffbda370baaa2cdf25c5207dbb78bc4aa0152
LeeJunSeo <77109803+ljs7143@users.noreply.github.com>
2022-10-26 22:23:45 +0900

    Create Utils.java

---
+++ ComputerLanguage/Utils.java
@@ -0,0 +1,30 @@
+package utils;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Utils {
+	static public int checkInputInteger(Scanner s, String msg) {
+		int n=0;
+		while(true) {
+			boolean valid = true; // check valid
+			System.out.print(msg);
+
+			try {
+				n = s.nextInt(); // take integer from the user
+			}
+			catch(InputMismatchException e) { // if error
+				System.out.println("Please input the number");
+				s.next(); // throws non-integer tokens in the input stream
+				valid = false;
+			}
+			finally {
+				if(valid) {
+					break;
+				}
+			}
+		}
+		
+		return n;
+	}
+}
