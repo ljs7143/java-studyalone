88ac2bfb1c8096d278a3f98769dfef748543f2bd
LeeJunSeo <77109803+ljs7143@users.noreply.github.com>
2022-10-28 22:22:42 +0900

    Create Grade.java

---
+++ ComputerLanguage/assignment3/Grade.java
@@ -0,0 +1,33 @@
+
+
+import java.util.Scanner;
+
+public class Grade {
+    int math; 
+    int science;
+    int english;
+
+    public Grade(int math, int science, int english){
+        this.math = math;
+        this.science = science;
+        this.english = english; 
+    }
+    public int average(){
+        return (math+science+english)/3;
+    }
+
+    public static void main(String [] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input the score of Math, Science, and English subject >> ");
+        int math = scanner.nextInt();
+        int science = scanner.nextInt();
+        int english = scanner.nextInt();
+        Grade me = new Grade(math, science, english);
+        System.out.println("Average is " + me.average()); 
+        scanner.close();
+    }
+    
+
+
+    
+}
