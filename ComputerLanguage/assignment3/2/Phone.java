3be227a18bcc710320a5e30e9cfc017c60b717f0
LeeJunSeo <77109803+ljs7143@users.noreply.github.com>
2022-10-28 23:01:36 +0900

    Create Phone.java

---
+++ ComputerLanguage/assignment3/2/Phone.java
@@ -0,0 +1,19 @@
+
+
+public class Phone {
+    public String name;
+    public String tel;
+
+    public Phone(String name, String tel){
+        this.name = name;
+        this.tel = tel;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String tel(){
+        return tel;
+    }
+}
