7c6cf1e5b12d2f953adaa327c282fe52eb9959fa
LeeJunSeo <77109803+ljs7143@users.noreply.github.com>
2022-10-31 22:38:49 +0900

    Update program.java

--- ComputerLanguage/assignment4/4/program.java
+++ ComputerLanguage/assignment4/4/program.java
@@ -1,30 +1,27 @@
 import java.util.Scanner;
 
 public class program {
-
     public static void main(String[] args) {
-        
-
         while(true){
             Scanner sc = new Scanner(System.in);
-            System.out.print(">>");
             String s = sc.nextLine();
+            System.out.print(">>");
+           
+            sc.nextLine();
             if(s.equals("exit")){
                 System.out.println("Exit.");
                 break;
             }
             int len = s.length();
             StringBuilder sb = new StringBuilder();
-            int random = (int)(Math.random()*len-1);
-            for(int i = 0; i<len; i++){
+            int random = (int)(Math.random()*len-2);
+            for(int i = 0; i<len-1; i++){
                 sb.append(s.charAt(i));
                 if(i==random){
                     sb.append((char)((int)(Math.random()*26)+97));
                 }
             }
             System.out.println(sb.toString());
-       
-            
         }
     }
 }
