f7faece3ec6dde8306fe9687c732cc051e2ef603
LeeJunSeo <77109803+ljs7143@users.noreply.github.com>
2022-11-01 22:45:13 +0900

    Create Location.java

---
+++ ComputerLanguage/assignment5/2/Location.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Location {
+
+    private String name;
+    private String latitude;
+    private String longitude;
+
+
+    public Location(String latitude, String longtitude){
+        this.latitude = latitude;
+        this.longitude = longtitude;
+    }
+}
