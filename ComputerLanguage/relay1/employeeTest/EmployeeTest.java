6ed5ff3e7677320d30933d424cfa8844ad5093f3
LeeJunSeo <77109803+ljs7143@users.noreply.github.com>
2022-11-03 19:18:47 +0900

    Update EmployeeTest.java

--- ComputerLanguage/relay1/employeeTest/EmployeeTest.java
+++ ComputerLanguage/relay1/employeeTest/EmployeeTest.java
@@ -1,15 +1,8 @@
-package ComputerLanguage.relay1.employeeTest;
+package ComputerLanguage.relay1;
 import java.util.Scanner;
-
-import ComputerLanguage.relay1.menus.MainMenuEnum;
-import ComputerLanguage.relay1.menus.Menu;
-import ComputerLanguage.relay1.menus.RegisterMenuEnum;
-
 public class EmployeeTest {
-
 	public static void main(String[] args) {
 		Menu menu = new Menu();
-
 		Scanner scanner = new Scanner(System.in);
 		while(true) {
 			MainMenuEnum mm = menu.MainMenu(scanner);
@@ -18,12 +11,27 @@ public class EmployeeTest {
 				while(true){
 					RegisterMenuEnum rm = menu.RegisterMenu(scanner);
 					if(rm == RegisterMenuEnum.FUllTIME ){
-						
+						System.out.println(">>> Fulltime Registration <<<");
+					}
+					else if(rm == RegisterMenuEnum.PARTTIME){
+						System.out.println(">>> Parttime Registration <<<");
+					}
+					else if(rm == RegisterMenuEnum.BACK){
+						break;
 					}
 				}
 			}
 			else if(mm == MainMenuEnum.SEARCH) {
 				System.out.println("Search");
+				while(true){
+					SearchMenuEnum sm = menu.SearchMenu(scanner);
+					if(sm == SearchMenuEnum.FUllSEARCH){
+						System.out.println(">>> Full Search <<<");
+					}
+					else if (sm == SearchMenuEnum.BACK){
+						break;
+					}
+				}
 			}
 			else if(mm == MainMenuEnum.EXIT) {
 				System.out.print("Good BYE!");
@@ -31,7 +39,7 @@ public class EmployeeTest {
 				break;
 			}
 		}
-		
 		scanner.close();
 	}
 }
+
